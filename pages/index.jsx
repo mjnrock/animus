578a4ff5fbbbc7b9c2a055985c52e5b5795893ee
mjnrock <mjnrock@umich.edu>
2019-07-04 21:27:54 -0400

    init

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default class Index extends React.Component {
+	componentDidMount(){
+		document.title = "dfsdfsdfsd"
+	}
+
+	render() {
+		return (
+			<div>Hello!</div>
+		);
+	}
+};
