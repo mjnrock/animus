f063813a5d27660bc852f4f899da61c4f0738f02
Matt Nawrocki <mjnrock@umich.edu>
2019-11-25 20:08:13 -0500

    init

---
+++ animus/package.js
@@ -0,0 +1,17 @@
+import AState from "./AState";
+import AAnimus from "./AAnimus";
+import Node from "./Node";
+import TimeNode from "./TimeNode";
+import Sequence from "./Sequence";
+
+import Plugin from "./plugin/package";
+
+export default {
+    AState,
+    AAnimus,
+    Node,
+    TimeNode,
+    Sequence,
+
+    Plugin
+};
